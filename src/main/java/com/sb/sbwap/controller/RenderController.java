85ac6b5b6e23c079ffb295a2922bf8a8d7848b20
高木　大介 <takagi-d@iij.ad.jp>
2025-05-19 19:27:44 +0900

    thymeleafでレンダリングした内容をJSONに載せて返却するサンプルを追加。

---
+++ src/main/java/com/sb/sbwap/controller/RenderController.java
@@ -0,0 +1,25 @@
+package com.sb.sbwap.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.sb.sbwap.dto.RenderItemsDto;
+
+import lombok.RequiredArgsConstructor;
+
+@Controller
+@RequiredArgsConstructor
+public class RenderController {
+
+	@GetMapping("/render")
+	public String getRender(Model model) {
+
+		// モデルに画面表示用のDTOを設定する
+		RenderItemsDto renderItemsDto = new RenderItemsDto();
+		model.addAttribute("renderItems", renderItemsDto);
+
+        // 遷移先画面名を呼び出し側に返却する
+        return "render";
+	}
+}
---
+++ src/main/java/com/sb/sbwap/dto/RenderItemsDto.java
@@ -0,0 +1,10 @@
+package com.sb.sbwap.dto;
+
+import lombok.Data;
+
+@Data
+public class RenderItemsDto {
+
+	private String inputValue;
+	private String renderError;
+}
